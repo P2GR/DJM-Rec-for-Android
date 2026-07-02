7e74870a491a8ad8352fc237e9c7d4e2d3ef558c
Lennart <lp@logboekenonline.nl>
2026-07-02 11:23:19 +0200

    Initial commit

---
+++ app/build/intermediates/cxx/RelWithDebInfo/b4p644g4/logs/arm64-v8a/build_command_targets.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\\Users\\lenna\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Web\\djmrec\\app\\.cxx\\RelWithDebInfo\\b4p644g4\\arm64-v8a" ^
+  benchmark_residual ^
+  djmrec_audio
